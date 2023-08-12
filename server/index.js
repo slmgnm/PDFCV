afe49f8f72b230bdd64cac51190b7db7201c056c
SALIM GHANEM <saleemghanem@gmail.com>
2023-08-12 22:07:58 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -3,23 +3,22 @@ const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 
-// const allowedOrigins = [
-//   "https://pdfcv-fe.vercel.app/",
-//   "http://localhost:3000",
-// ];
+const allowedOrigins = [
+  "https://pdfcv-fe.vercel.app/",
+  "http://localhost:3000",
+];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (allowedOrigins.includes(origin) || !origin) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
